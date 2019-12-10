c366be8b503bdebfdca722f3b8b52ad27e672274
Meowth0 <41973900+Meowth0@users.noreply.github.com>
2019-12-10 22:18:51 +0300

    dependencyconfig

---
+++ DependencyInjection/DependencyInjection/ExampleClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+ 
+    public interface IRepository
+    {
+        int GetNum();
+    }
+    public class MySQLRepository : IRepository
+    {
+        public int GetNum()
+        {
+            return 111;
+        }
+    }
+}
---
+++ DependencyInjection/DependencyInjection/ImplementationInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    public class ImplementationInfo
+    {
+        private object instance;       
+        private static object syncRoot;
+        public Type implementationType { get; }
+        public bool isSingleton { get; }
+
+        public ImplementationInfo(Type dependencyType, bool isSingleton)
+        {
+            implementationType = dependencyType;
+            this.isSingleton = isSingleton;
+            syncRoot = new object();
+            instance = null;
+        }
+    }
+}
